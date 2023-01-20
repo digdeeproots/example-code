2cd016c2745660f6c51144162359fe2137551d86
arlobelshee <arlo@belshee.com>
2023-01-20 14:04:43 -0500

    F - some test helpers.

--- src/testhelpers.py
+++ src/testhelpers.py
@@ -1,3 +1,23 @@
+import random
+
+
 def test(f):
     f.__test__ = True
     return f
+
+
+class ARBITRARY:
+    next_value = 100
+
+    @staticmethod
+    def many_dollars():
+        return 1000 + random.randint(0, 100)
+
+    @staticmethod
+    def few_dollars():
+        return random.randint(10, 50)
+
+    @staticmethod
+    def account_name():
+        ARBITRARY.next_value += 1
+        return f"test account:{ARBITRARY.next_value}"
